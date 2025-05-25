b7ca6d2e32b88cee188371ebe5c9793ade8ea0a9
Denise <denisehsx01@gmail.com>
2025-05-25 19:34:23 +0530

    Hero: Fix 2
    The MapPin icon in the location input field has been adjusted to align vertically centered inside the input box by using CSS classes: top-1/2 transform -translate-y-1/2. This ensures the icon is properly aligned with the text input.

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -198,7 +198,8 @@ const Index = () => {
                 {/* Number of Guests */}
                 <div className="flex flex-col flex-grow-0 min-w-[180px]">
                   <div className="relative">
-                    <Users className="absolute left-3 top-10 h-5 w-5 text-gray-400" />
+                    <Users className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400" />
+
                     <select
                       className="w-full md:w-48 h-12 pl-10 pr-4 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-primary focus:border-primary"
                       value={capacity}
